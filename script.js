fff4f5678004856de50c1c841fb3b0c238105ede
Anderson-Schi <jr93@hotmai.com>
2022-11-18 00:13:03 +0000

    exercicios aula lacos 1 e exercicos propostos

---
+++ script.js
@@ -0,0 +1,54 @@
+//ELEMENTOS NECESSÁRIOS
+/* Inicialização de váriável*/
+/* Condição de continuação*/
+/* Um conjunto de ações*/
+/* Uma alteração que mude a condição de continuação*/
+
+/* let numero = Number(prompt("Adivinhe um numero"));
+let soma  = 0
+
+while(numer !== 0){
+    soma+= numero
+    numero = Number(prompt("Tente outro numero numero"));
+};
+console.log(`Acertou o numero e a soma de todos os numeros e ${soma}`); */
+
+/* let num = Number(prompt("Insira um numero: "));
+
+for(let i = 0; i <= num; i++){
+    console.log(`${i}`)
+};
+
+console.log(`Fim do laco for`); */
+
+/* const numeros = [2,4,6,8,10];
+
+
+for(indice = 0; indice < numeros.length; indice++ ){
+    console.log(`O numero do indice ${indice} e ${numeros[indice]}`);
+}; */
+
+// para guardar na cabeca
+// 1)
+ /* let tipoUsuario = prompt("Insira seu tipo de usuario:\n A - ADM \n B - Usuario comum \n C - Assinante").toUpperCase();
+
+while(tipoUsuario !== "A" ){
+    console.log(`Acesso Negado, tente novamente !!`);
+    tipoUsuario = prompt("Insira seu tipo de usuario:\n A - ADM \n B - Usuario comum \n C - Assinante").toUpperCase();
+};
+console.log(`Boas-vindas Adm !`);  */
+
+// 2)
+/* const num = Number(prompt("Digite um numero"));
+
+for(i=1; i <= 10 ; i++){
+    console.log(`${num} x ${i} = ${num*i}`)
+}; */
+
+// 3)
+
+const meuArray = ["um", "dois", "tres"];
+
+for(i = 0; i <= meuArray.length ; i++){
+    console.log(meuArray[i].toUpperCase());
+};
